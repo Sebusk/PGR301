be892852830657d879718cc7db0de0d7926e99b1
Sebastian <seb.knutsen@gmail.com>
2018-09-03 12:35:25 +0200

    inital hello-world commit

---
+++ hello-world/src/main/java/no/woact/knuseb16/pgr301/App.java
@@ -0,0 +1,13 @@
+package no.woact.knuseb16.pgr301;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
